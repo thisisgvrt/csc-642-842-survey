68202ae0ee18c5a7c2c4705f229f83770410a310
Raviteja Guttula <thisisgvrt@gmail.com>
2020-07-17 19:06:16 -0700

    Fixed month related changes

--- src/App.js
+++ src/App.js
@@ -159,7 +159,7 @@ function App() {
                 Month
               </label>
               <div className="relative">
-                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="month-of-birth">
+                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="month-of-birth" value={monthOfBirth} onChange={event => setMonthOfBirth(event.target.value)}>
                   <option>January</option>
                   <option>Febraury</option>
                   <option>March</option>
