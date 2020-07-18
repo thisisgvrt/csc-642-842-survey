2d792d0268eb6c499778a11b8dd2fd3d534a1457
Raviteja Guttula <thisisgvrt@gmail.com>
2020-07-17 19:21:53 -0700

    Fixed bug with date

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
   if (isNaN(parseInt(dateOfBirth))) {
     isDateOfBirthValid = (dateOfBirth === "")
   } else {
-    isDateOfBirthValid = (parseInt(dateOfBirth) > 0 && parseInt(dateOfBirth) < 31)
+    isDateOfBirthValid = (parseInt(dateOfBirth) > 0 && parseInt(dateOfBirth) <= 31)
   }
 
   const [monthOfBirth, setMonthOfBirth] = useState("");
@@ -265,7 +265,7 @@ function App() {
 
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <button class={"bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-not-allowed focus:outline-none " + (isRecaptachaCompleted ? "focus:shadow-outline  hover:bg-blue-700": "")} type="button">
+              <button class={"bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none " + (isRecaptachaCompleted ? "focus:shadow-outline  hover:bg-blue-700": "cursor-not-allowed")} type="button">
                 Submit
               </button>
             </div>
